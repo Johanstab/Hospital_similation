ca68a36cf002e93785254b19e66aaf0b5d236530
Sabina <sababina@hotmail.com>
2022-04-26 09:25:50 +0200

    fix

---
+++ Simuleringsmodell/stue.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Johan Stabekk, Sabina Langås'
+__email__ = 'johan.stabekk@nmbu.no, sabina.langas@nmbu.no'
+
+
+class Stue:
+
+    def __init__(self, navn, hoytid=False):
+
+        self.navn = navn
+
+        if not hoytid:
+            self.skift_1 = 480 * 1.3
+            self.skift_2 = 150 * 1.3
+            self.skift_3 = 270 * 1.3
+            self.skift_4 = 540 * 1.3
+
+        else:
+            self.skift_1 = 4000 * 4
+            self.skift_2 = 800  * 4
+            self.skift_3 = 1000 * 4
+            self.skift_4 = 400  * 4
+
+    def get_time(self, skift):
+
+        if skift == 1:
+            return self.skift_1
+        elif skift == 2:
+            return self.skift_2
+        elif skift == 3:
+            return self.skift_3
+        elif skift == 4:
+            return self.skift_4
+
+    def fast_tid(self, skift):
+
+        if skift == 1:
+            return 480 * 1.3
+        elif skift == 2:
+            return 150 * 1.3
+        elif skift == 3:
+            return 270 * 1.3
+        elif skift == 4:
+            return 540 * 1.3
+
+    def fast_tid_delt(self, skift):
+
+        if skift == 1:
+            return 480 * 1.3
+        elif skift == 2:
+            return 150 * 1.3
+        elif skift == 3:
+            return 270 * 1.3
+        elif skift == 4:
+            return 540 * 1.3
+
+    def update(self, skift, value):
+        if skift == 1:
+            self.skift_1 += -value - 45
+        elif skift == 2:
+            self.skift_2 += -value - 30
+        elif skift == 3:
+            self.skift_3 += -value - 30
+        elif skift == 4:
+            self.skift_4 += -value - 30
+
+    def reset_stue(self, hoytid=False):
+
+        if not hoytid:
+            self.skift_1 = 480 * 1.3
+            self.skift_2 = 150 * 1.3
+            self.skift_3 = 270 * 1.3
+            self.skift_4 = 540 * 1.3
+
+    def test_time(self,skift, value):
+
+        tid_1 = self.skift_1
+        tid_2 = self.skift_2
+        tid_3 = self.skift_3
+        tid_4 = self.skift_4
+
+        if skift == 1:
+            tid_1 += -value - 45
+            return  tid_1
+        elif skift == 2:
+            tid_2 += -value - 30
+            return tid_2
+        elif skift == 3:
+            tid_3 += -value - 30
+            return tid_3
+        elif skift == 4:
+            tid_4 += -value - 30
+            return tid_4
+
---
+++ Simuleringsmodell/sykehus.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Johan Stabekk, Sabina Langås'
+__email__ = 'johan.stabekk@nmbu.no, sabina.langas@nmbu.no'
+
+
+class Sykehus:
+
+    def __init__(self):
+        pass
+
+    def fordel_trafikklys(self, pasient_liste, Diagnose_df):
+
+        for pasient in pasient_liste:
+            for index, row in Diagnose_df.iterrows():
+                if pasient.diagnose == row['DiagnoseGruppe'] and row['Hastegrad'] == 'Rød':
+                    pasient.hast = 'Red'
+                    pasient.hast_nummer = 3 # Denne la jeg til for å sortere lettere :)
+                    break
+                elif pasient.diagnose == row['DiagnoseGruppe'] and row['Hastegrad'] == 'Gul':
+                    pasient.hast = 'Yellow'
+                    pasient.hast_nummer = 2
+                    break
+                elif pasient.diagnose == row['DiagnoseGruppe'] and row['Hastegrad'] == 'Grønn':
+                    pasient.hast = 'Green'
+                    pasient.hast_nummer = 1
+                    break
+                else:
+                    continue
+
+    def fikser_dager(self, pasient_liste):
+
+        for pasient in pasient_liste:
+            if pasient.tid == 'Dag':
+                pasient.tid = 1
+            elif pasient.tid == 'Tidlig Kveld':
+                pasient.tid = 2
+            elif pasient.tid == 'Kveld':
+                pasient.tid = 3
+            elif pasient.tid == 'Natt':
+                pasient.tid = 4
+
+    def legge_til_uker(self, pasient_liste):
+
+        counter = 1
+
+        for month in range(1, 13):
+            for day in range(7):
+                dag = []
+                tidligkveld = []
+                kveld = []
+                natt = []
+                slutt_liste = []
+
+                for pasient in pasient_liste:
+
+                    if pasient.month == month:
+
+                        if pasient.ukedag == day:
+
+                            if not pasient.behandlet:
+
+                                if pasient.tid == 1:
+                                    dag.append(pasient)
+
+                                elif pasient.tid == 2:
+                                    tidligkveld.append(pasient)
+
+                                elif pasient.tid == 3:
+                                    kveld.append(pasient)
+
+                                elif pasient.tid == 4:
+                                    natt.append(pasient)
+
+                dag.sort(key=lambda x: x.hast_nummer, reverse=True)
+                tidligkveld.sort(key=lambda x: x.hast_nummer, reverse=True)
+                kveld.sort(key=lambda x: x.hast_nummer, reverse=True)
+                natt.sort(key=lambda x: x.hast_nummer, reverse=True)
+
+                slutt_liste.extend(dag)
+                slutt_liste.extend(tidligkveld)
+                slutt_liste.extend(kveld)
+                slutt_liste.extend(natt)
+
+                for i in slutt_liste:
+                    i.uke = counter
+
+                    if counter < 4:
+                        counter += 1
+                    else:
+                        counter = 1
